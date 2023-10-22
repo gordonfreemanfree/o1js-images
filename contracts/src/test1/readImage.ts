2734e44083c97de302b4f8d4505936c39a034b51
gordonfreemanfree <35875803+gordonfreemanfree@users.noreply.github.com>
2023-10-22 22:37:01 +0200

    experimenting and learning

--- contracts/src/test1/readImage.ts
+++ contracts/src/test1/readImage.ts
@@ -1,16 +1,19 @@
 import fs from 'fs';
-
+import { ImageClass } from './class1.js';
+import { Poseidon } from 'o1js';
 // image in this folder
 // path is relative to the file
-const filePath = '../../../assets/test1.jpg';
+const filePath = '../../../assets/test1.jpeg';
 
 fs.readFile(filePath, 'base64', (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(data);
-});
+  // convert buffer to matrix
 
-// convert image to matrix
-// https://www.npmjs.com/package/image-to-matrix
+  console.log(data);
+  let test = new ImageClass(data);
+  console.log(test);
+  return ImageClass;
+});
