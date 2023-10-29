6121c1d591585d8b9fb2e5db0631bdba92824cc5
gordonfreemanfree <35875803+gordonfreemanfree@users.noreply.github.com>
2023-10-29 15:34:35 +0100

    cleaning up and scaling up

--- contracts/src/MVP/test1.ts
+++
@@ -1,6 +0,0 @@
-// import { ImageClass8x8, ImageSize8x8 } from './imageClass';
-// import { preprocessImage } from './utils/preprocessImage';
-// import { image_1_label_2_8x8 } from '../../assets/image_1_label_2_8x8';
-
-// let test = new ImageSize8x8(preprocessImage(image_1_label_2_8x8));
-// console.log(test);
